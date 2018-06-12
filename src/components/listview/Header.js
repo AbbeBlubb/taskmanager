c7303d09343a6c5653fc0e0d4df53d121379e4f8
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-06-12 21:01:31 +0200

    Content and styling for Header

--- src/components/listview/Header.js
+++ src/components/listview/Header.js
@@ -1,16 +1,19 @@
 import React from "react"
+import "./header.css"
+import user from "../../media/user.png"
+import edit from "../../media/edit.png"
 
 export default class Header extends React.Component {
 
   render() {
     return (
       <header className="listheader">
-        <div className="back">
-          Tillbakapil
+        <div className="user">
+          <img src={user} alt="You are logged in!" />
         </div>
         <div className="title">Task Manager</div>
         <div className="settings">
-          Kontroller
+          <img src={edit} alt="Create your own task" />
         </div>
       </header>
     )
