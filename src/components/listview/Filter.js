9aa9f55c018ba3a7dc21c5905866f9b59e64fea2
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-06-12 21:14:24 +0200

    Content and styling for Filter (no filtering function)

--- src/components/listview/Filter.js
+++ src/components/listview/Filter.js
@@ -1,16 +1,18 @@
 import React from "react"
+import loupe from "../../media/loupe.png"
+import "./filter.css"
 
 export default class Filter extends React.Component {
 
   render() {
     return (
       <section className="filter">
-        <div>
+        <div className="container">
           <div>
             Låda för sökning
           </div>
-          <div>
-            Bild på förstoringsglas
+          <div className="search">
+            <img src={loupe} alt="Search" />
           </div>
         </div>
       </section>
