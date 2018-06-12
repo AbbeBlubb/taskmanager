bfa2682070153acff8916c346d5370cff77ff6b5
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-06-12 22:14:29 +0200

    Fetches and sets state, works. If-else for render in case of slow fetch.

--- src/components/listview/List.js
+++ src/components/listview/List.js
@@ -2,12 +2,40 @@ import React from "react"
 
 export default class List extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      informationIsDownloaded: false
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response => (
+      response.json()
+    )).then(json => {
+      this.setState({
+        users: json,
+        informationIsDownloaded: true
+      })
+      console.log(this.state.users)
+    })
+  }
+
   render() {
-    return (
-      <section className="list">
-        Mappa ut filtrerad array
-      </section>
-    )
+    if (!this.state.informationIsDownloaded) {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    } else {
+      return (
+        <section className="list">
+          Mappa ut filtrerad array
+        </section>
+      )
+    }
   }
 
 }
