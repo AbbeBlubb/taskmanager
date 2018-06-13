6fc259b67252ca8adac66f0b2a7b49c0fdb413c3
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-06-13 17:17:03 +0200

    Hero img as background, arrow as back button

--- src/components/taskview/Hero.js
+++ src/components/taskview/Hero.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./hero.css"
+import leftarrow from "../../media/left-arrow.png"
 
 export default class Hero extends React.Component {
 
@@ -10,24 +11,24 @@ export default class Hero extends React.Component {
         <div className="container">
           <div className="back">
             <Link to="/">
-              Tillbakaknapp
+              <img src={leftarrow} alt="Back" />
             </Link>
           </div>
           <div className="userinfo">
             <div>
-              <div>
+              <div className="large">
                 {this.props.name
                   ? this.props.name
-                  : `User number ${this.props.userId}`}
+                  : `User id: ${this.props.userId}`}
               </div>
-              <div>
+              <div className="medium">
                 {this.props.nick
                   ? this.props.nick
                   : null}
               </div>
-            </div>
-            <div>
-              Antal saker kvar att göra
+              <div className="medium">
+                Antal saker kvar - callback
+              </div>
             </div>
           </div>
         </div>
