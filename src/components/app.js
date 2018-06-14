4a3741ab428ef012ba8302e053c020499ef14640
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-06-14 13:35:22 +0200

    For search in Filter: App has now state for the input value, that is set by function handleChangeForSearchUser. Function sent to Filter as props

--- src/components/app.js
+++ src/components/app.js
@@ -7,12 +7,25 @@ import TaskView from "./taskview/TaskView"
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputSearchUser: ""
+    }
+  }
+
+  handleChangeForSearchUser = event => {
+    this.setState({
+      inputSearchUser: event.target.value
+    })
+  }
+
   render() {
     return (
       <HashRouter>
         <div className="main">
           <Route exact path="/" component={Header} />
-          <Route exact path="/" component={Filter} />
+          <Route exact path="/" render={() => <Filter handleChange={this.handleChangeForSearchUser} />}  />
           <Route exact path="/" component={UserList} />
           <Route exact path="/user/:id" component={TaskView} />
         </div>
