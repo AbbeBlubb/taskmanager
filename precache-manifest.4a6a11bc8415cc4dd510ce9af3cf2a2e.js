71963f8f1fdc5909174b7ac9d6e721d9069f6c2e
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-02-14 00:22:13 +0100

    Updates

---
+++ precache-manifest.4a6a11bc8415cc4dd510ce9af3cf2a2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1402ac5b560134a5bc5e",
+    "url": "/taskmanager/static/css/main.9cc5d415.chunk.css"
+  },
+  {
+    "revision": "1402ac5b560134a5bc5e",
+    "url": "/taskmanager/static/js/main.1402ac5b.chunk.js"
+  },
+  {
+    "revision": "1cc0018d4be7be2a25ba",
+    "url": "/taskmanager/static/js/1.1cc0018d.chunk.js"
+  },
+  {
+    "revision": "6a0307bc322e9402fb1d",
+    "url": "/taskmanager/static/js/runtime~main.6a0307bc.js"
+  },
+  {
+    "revision": "9c5ad19eacbf3928f0ec69fd797dfc07",
+    "url": "/taskmanager/static/media/user.9c5ad19e.png"
+  },
+  {
+    "revision": "0d7414cf0ddf787c01f358de25c61ef5",
+    "url": "/taskmanager/static/media/right-arrow-1.0d7414cf.png"
+  },
+  {
+    "revision": "b78908624e03515259325ef401713b3f",
+    "url": "/taskmanager/static/media/clock.b7890862.png"
+  },
+  {
+    "revision": "a254c645ca5de3b0547e5fea5de8319e",
+    "url": "/taskmanager/static/media/background-board-book-733854.a254c645.jpg"
+  },
+  {
+    "revision": "ced5d3934b1f3d12d3235a229c757166",
+    "url": "/taskmanager/index.html"
+  }
+];
