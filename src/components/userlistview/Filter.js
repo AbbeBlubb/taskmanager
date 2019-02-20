9d559a73a6b32109b9127b72b4995b7f1f1744e2
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-02-20 22:34:14 +0100

    Filter input placeholder styling

--- src/components/userlistview/Filter.js
+++ src/components/userlistview/Filter.js
@@ -7,8 +7,11 @@ export default class Filter extends React.Component {
     return (
       <section className="filter">
         <input
-          autoFocus="{true}"
           className="filter__search"
+          autoFocus="{true}"
+          placeholder="Search user"
+          onFocus={(event) => event.target.placeholder = ''}
+          onBlur={(event) => event.target.placeholder = 'Search user'}
           onChange={event => this.props.handleChange(event)} />
         <img className="icon" src={loupe} alt="Search user information: name, username, e-mail, or city" title="Search user information: name, username, e-mail, or city" />
       </section>
