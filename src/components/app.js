449ada4457a04863d1b428eada15d7d163cf8a94
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-02-23 18:31:46 +0100

    ScrollToTop

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { HashRouter, Route } from "react-router-dom"
+import ScrollToTop from "./ScrollToTop"
 import UserListView from "./userlistview/UserListView"
 import TaskListView from "./tasklistview/TaskListView"
 import '../assets/waves'
@@ -50,10 +51,10 @@ class App extends React.Component {
   render() {
     return (
       <HashRouter>
-        <div className="main">
+        <ScrollToTop>
           <Route exact path="/" component={UserListView} />
           <Route exact path="/user/:id" component={TaskListView} />
-        </div>
+        </ScrollToTop>
       </HashRouter>
     )
   }
